e0b26e4d0f9314bd13f94494e41116f455c52390
NibinByteCode <nibinm365@gmail.com>
2024-06-06 03:11:50 -0400

    Menu Items Insert Query

---
+++ DinePulse_SQLServer/DinePulse_DML_2sql.sql
@@ -0,0 +1,136 @@
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Blackberry Mojito', 3, 'Sip on the refreshing symphony of tangy lime juice, effervescent soda water, and a touch of sweetness from syrup, harmonizing flawlessly with succulent blackberries, fresh mint, and spirited rum in a blackberry mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Blueberry Mojito', 3, 'Delight in the vibrant medley of zesty lime juice, sparkling soda water, and a delicate hint of sweetness from syrup, perfectly complementing the juicy blueberries, crisp mint, and invigorating rum in a blueberry mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Coconut Mojito', 3, 'Embark on a tropical journey with the harmonious blend of zesty lime juice, effervescent soda water, and a subtle sweetness from syrup, all enhancing the creamy coconut essence, fresh mint, and spirited rum in a coconut mojito.', 8.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Mango Mojito', 3, 'Savor the tropical delight of zesty lime juice, fizzy soda water, and a touch of sweetness from syrup, perfectly complementing the luscious mango, fresh mint, and spirited rum in a mango mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Strawberry Mojito', 3, 'Indulge in the perfect balance of tangy lime juice, fizzy soda water, and a hint of sweetness from syrup, all complementing the vibrant strawberries, fresh mint, and spirited rum in a strawberry mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Pineapple Mojito', 3, 'Transport yourself to a sunny paradise with the zing of lime juice, the fizz of soda water, and a hint of sweetness from syrup, all accentuating the tropical burst of pineapple, fresh mint, and spirited rum in a pineapple mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Peach Mojito', 3, 'Embrace the essence of summer with the tangy lime juice, effervescent soda water, and subtle sweetness from syrup, perfectly enhancing the juicy peach, fresh mint, and spirited rum in a peach mojito.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Vodka Mojito', 3, 'Experience a refreshing twist with zesty lime juice, sparkling soda water, and a hint of sweetness from syrup, blending harmoniously with the crisp vodka, fresh mint, and a burst of flavor in a vodka mojito.', 13.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Classic Mojito', 3, 'A classic Mojito is a refreshing cocktail made with white rum, fresh mint leaves, lime juice, sugar, and soda water, typically served over ice.', 9.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Fresh Lemon Soda', 3, 'Refreshing and zesty, Fresh Lemon Soda is a delightful blend of tangy lemon juice and fizzy soda, creating a revitalizing and thirst-quenching food that`s perfect for any occasion.', 6.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Rainbow Smoothie', 4, 'Indulge in a vibrant rainbow smoothie with layers of antioxidant-rich fruits and creamy Greek yogurt, each adding a burst of color and flavor to this delightful treat!', 10.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Immune Wellness Smoothie', 4, 'A delicious blend of ginger, turmeric, spinach, pineapple, banana, honey, and black pepper, perfect for a refreshing breakfast or snack, offering digestion support and anti-inflammatory benefits when you`re feeling under the weather.', 10.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Acai Smoothie', 4, 'Indulge in a luscious acai smoothie, blending the richness of acai pulp with a zesty twist from lime juice, complemented by the tropical sweetness of frozen pineapple and mango, perfectly balanced with the creamy texture and natural sweetness of banana.', 10.00, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Almond Butter Smoothie', 4, 'Savor the creamy delight of an almond butter smoothie, blending rich almond butter with frozen pineapple, banana, and comforting spices, enriched with vanilla extract for a delightful treat.', 11.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Avocado Banana Smoothie', 4, 'Savor the creamy blend of ripe avocado, green apple, banana, and frozen pineapple in this refreshing almond avocado smoothie, enhanced with leafy greens, lemon juice, and ice for a delightful and nourishing treat.', 10.00, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Grape Smoothie', 4, 'Delight in the sweet simplicity of an almond grape smoothie, blending juicy red grapes with a room temperature banana for creaminess, protein-packed Greek yogurt, and your choice of milk for a refreshing and nourishing treat.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Oat Milk Smoothie', 4, 'Enjoy the creamy blend of frozen strawberries, fresh banana, oat milk, and optional sweetness from maple syrup or honey in this refreshing almond oat milk smoothie.', 10.00, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Arnold Palmer Tea', 4, 'Sip on the timeless refreshment of an Arnold Palmer, blending three parts unsweetened iced tea with one part lemonade for the perfect balance of briskness and tang.', 6.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Iced White Chocolate Mocha', 4, 'Savor the irresistible blend of rich espresso, velvety milk, and decadent white chocolate, served over ice and topped with creamy cold foam or whipped cream promising a symphony of indulgent flavors with every sip.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Iced Oat Milk Espresso', 4, 'Delight in the refreshing blend of shaken espresso, creamy oat milk, and indulgent brown sugar syrup in Starbucks` Iced Brown Sugar Oat Milk Espresso, a tantalizing twist on the classic iced latte, boasting a frothy texture and nuanced sweetness.', 8.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Iced Tea', 4, 'Quench your thirst with the timeless refreshment of iced tea—a harmonious infusion of carefully steeped tea leaves, served chilled over ice, offering a crisp and revitalizing beverage for any occasion.', 6.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Iced Green Tea', 4, 'Quench your thirst and invigorate your senses with the refreshing simplicity of iced green tea, marrying the herbaceous notes of green tea with a touch of lemon and the delightful chill of ice', 6.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Matcha Latte', 4, 'Indulge in the serene simplicity of a matcha latte, where vibrant matcha paste is delicately whisked into frothy water, sweetened with a touch of honey or maple syrup, then enveloped in creamy steamed milk,', 8.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Vanilla Chai Latte', 4, 'Savor the exquisite blend of warming spices including cinnamon, cardamom, and ginger, combined with the velvety frothed milk foam of a classic cafe latte, in the indulgent vanilla chai latte', 8.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Cappuccino', 4, 'Experience the harmonious balance of espresso, velvety steamed milk, and airy foam in a cappuccino, where each sip offers a symphony of rich flavors and creamy textures.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Lemon Chicken Orzo Soup', 1, 'Indulge in the comforting embrace of lemon chicken orzo soup, where tender shredded chicken and fluffy orzo pasta mingle harmoniously in a zesty broth infused with the freshness of lemon, garlic, oregano, and dill.', 9.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Paneer Pudina Tikka', 1, 'Paneer Pudina Tikka is a flavorful Indian appetizer where succulent paneer (cottage cheese) cubes are marinated in minty spices and grilled to perfection, offering a delightful blend of freshness and taste.', 15.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Loaded Nachos', 1, 'Delicious loaded nachos topped with melted cheese, tangy salsa, creamy guacamole, and zesty jalapenos, creating a mouthwatering fusion of flavors and textures. Perfect for sharing and indulging.', 11.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Banana Oatmeal Pancakes', 1, 'These healthy banana oatmeal pancakes are flourless and made in a blender for quick and easy breakfasts! Enjoy fluffy, satisfying pancakes packed with wholesome oats and natural sweetness.', 11.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Vegan Crab Cakes', 1, 'Indulge in these incredible vegan crab cakes, crafted with hearts of palm, artichokes, and chickpeas, offering a delightful plant-based dinner or appetizer paired with tangy tartar sauce.', 10.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Garbanzo Bean Salad', 1, 'Enjoy the vibrant medley of flavors in this Garbanzo Bean Salad, featuring protein-packed beans, colorful bell peppers, aromatic shallots, fresh parsley, and a savory sprinkle of cheese', 10.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Creamy Gemelli Pasta', 1, 'Delight in the luxurious comfort of creamy gemelli pasta, where the unique corkscrew shape embraces the richness of a garlicky Parmesan cream sauce, while tender spinach adds a vibrant pop of color and wholesome goodness.', 11.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Tagliatelle Pasta', 1, 'Delight in the comforting allure of tagliatelle pasta—a long, delightfully chewy noodle that offers a satisfying bite, perfect for a hearty pasta dinner. Paired with a tangy homemade marinara sauce, fresh basil, and Parmesan', 11.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Breaded Tofu Sandwich', 1, 'Savor the sensational flavors of this vegan delight, featuring crispy breaded tofu, zesty Russian dressing, and crisp pickles, harmoniously layered between slices of bread for a mouthwatering sandwich experience that delights the senses.', 11.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Veggie Wrap', 1, 'Wrap up a symphony of flavors and colors with this delightful veggie wrap, featuring savory chickpea salad, vibrant vegetables, and a delectable spread, creating a lunch option that`s bound to evoke envy and satisfaction in every bite.', 8.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Cottage Cheese Bowl', 1, 'Elevate your mornings with Cottage Cheese Breakfast Bowls: a modern twist on a classic, combining creamy cottage cheese with fresh fruit, cinnamon, and honey for a satisfying and protein-packed start to your day.', 12.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Karari Roomali', 2, 'Karari Roomali is a popular Indian appetizer. Thin, crispy bread served folded, stuffed, or plain, often accompanied by chutneys. Delightfully satisfying street food with diverse fillings and flavors.', 13.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Blackened Chicken', 2, 'Experience the tantalizing flavor and crispy texture of blackened chicken, cooked to perfection in a cast iron skillet with a savory blend of Cajun-style spices', 15.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Cheese Enchiladas', 2, 'Satisfy your cravings with these mouthwatering cheese enchiladas, brimming with gooey cheese, enveloped in warm tortillas, and bathed in a zesty enchilada sauce - a simple yet delightful dinner option guaranteed to please everyone at the table.', 12.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Chicken and Rice', 2, 'Experience the ultimate comfort of this one-pot chicken and rice dish—a harmonious blend of tender chicken thighs and fluffy rice simmered in a flavorful broth, elevated with a vibrant parsley and lemon gremolata topping', 11.29, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Lemon Salmon', 2, 'Indulge in the exquisite flavors of this easy baked lemon salmon, delicately roasted with zesty lemon, aromatic garlic, and fragrant dill, all coming together to create a tender and flavorful dish in just 20 minutes', 10.49, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Shrimp Linguine', 2, 'Immerse yourself in the exquisite flavors, where plump, succulent shrimp intertwine with al dente pasta, enveloped in a vibrant garlic cream sauce, creating a satisfying and effortlessly elegant dinner experience.', 13.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Tofu Bowl', 2, 'Indulge in the deliciously wholesome goodness of this tofu bowl, where crispy tofu takes center stage alongside fresh veggies, spicy mayo, and crunchy fried onion bits, creating a delectable vegan dinner bursting with flavor and nourishment.', 12.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Tofu Fried Rice', 2, 'Delight in the comforting embrace of tofu fried rice, where tender tofu harmonizes with fluffy rice, vibrant vegetables, and a savory sauce, crafting a hearty and satisfying vegan dinner experience.', 13.99, NULL);
+
+INSERT INTO tbl_menu (item_name, item_category, item_description, item_price, item_image) 
+VALUES ('Vegan Sushi', 2, 'Experience the artistry of vegan sushi, where savory marinated tofu, crunchy vegetables, and tender sushi rice come together in a symphony of flavors, enhanced by a delectable Japanese BBQ Sauce topping.', 13.49, NULL);
+
+
