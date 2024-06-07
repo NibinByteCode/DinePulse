ba44967be51814c889908599a5afb4c31165b1f7
NibinByteCode <nibinm365@gmail.com>
2024-06-06 22:52:58 -0400

    Add functionality of save and retrieve image for category

--- DinePulse_API/DinePulse_API/Controllers/MenuCategoryController.cs
+++ DinePulse_API/DinePulse_API/Controllers/MenuCategoryController.cs
@@ -20,42 +20,35 @@ namespace DinePulse_API.Controllers
             dataLayer = new DataLayer(_iconfiguration);
         }
         [HttpGet]
-        [ActionName("GetCategory")]
-        public IActionResult GetMenuCategory()
+        [ActionName("GetAllMenuCategories")]
+        public IActionResult GetAllMenuCategories()
         {
             try
             {
-                DataTable table = new DataTable();
-                table = dataLayer.Getfromdb("MenuCategory_GetCategory");
-                if (table.Rows.Count > 0)
-                {
-                    string JSONresult;
-                    JSONresult = Utils.JsonHelper.DataTableToJsonObj(table);
-                    if (JSONresult != null)
-                    {
+                DataTable table = dataLayer.Getfromdb("MenuCategory_GetCategory");
 
-                        return Ok("{\"data\":" + JSONresult + "}");
-                    }
-                    else
-                    {
-                        return BadRequest("No data");
-                    }
-                }
-                else
+                List<CategoryModel> categories = new List<CategoryModel>();
+                foreach (DataRow row in table.Rows)
                 {
-                    return BadRequest("No data");
+                    CategoryModel category = new CategoryModel
+                    {
+                        CategoryId = Convert.ToInt32(row["category_id"]),
+                        CategoryName = row["category_name"].ToString(),
+                        CategoryDescription = row["category_description"].ToString(),
+                        CategoryImageBase64 = Convert.ToBase64String(row["category_image"] as byte[])
+                    };
+
+                    categories.Add(category);
                 }
 
-
+                return Ok(categories);
             }
-
             catch (Exception ex)
             {
-                new LogHelper().LogError("Error getting data..." + ex.Message);
-                return BadRequest("No Data Fetched...Please Try Later");
+                new LogHelper().LogError("Error retrieving menu categories: " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
             }
         }
-
 
         [HttpPost]
         [ActionName("InsertCategory")]
@@ -95,6 +88,85 @@ namespace DinePulse_API.Controllers
             {
                 // Log the error
                 new LogHelper().LogError("Error inserting category: " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpPut]
+        [ActionName("UpdateMenuCategory")]
+        public async Task<IActionResult> UpdateMenuCategory([FromForm] CategoryModel categoryModel, [FromForm] IFormFile categoryImage)
+        {
+            try
+            {
+                byte[] imageBytes = null;
+                if (categoryImage != null && categoryImage.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        await categoryImage.CopyToAsync(ms);
+                        imageBytes = ms.ToArray();
+                    }
+                }
+                else if (!string.IsNullOrEmpty(categoryModel.CategoryImageBase64))
+                {
+                    imageBytes = Convert.FromBase64String(categoryModel.CategoryImageBase64);
+                }
+
+                List<SqlParameter> parameters = new List<SqlParameter>
+        {
+            new SqlParameter("@category_id", SqlDbType.Int) { Value = categoryModel.CategoryId },
+            new SqlParameter("@category_name", SqlDbType.VarChar, 50) { Value = categoryModel.CategoryName },
+            new SqlParameter("@category_description", SqlDbType.VarChar, 255) { Value = categoryModel.CategoryDescription },
+            new SqlParameter("@category_image", SqlDbType.VarBinary, -1) { Value = (object)imageBytes ?? DBNull.Value }
+        };
+
+                int rowsAffected = dataLayer.ExecuteSp_transaction("MenuCategory_UpdateCategory", parameters);
+
+                if (rowsAffected > 0)
+                {
+                    return Ok("Menu category updated successfully.");
+                }
+                else
+                {
+                    return BadRequest("Failed to update menu category.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                new LogHelper().LogError("Error updating menu category: " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+
+        [HttpDelete("{categoryId}")]
+        [ActionName("DeleteMenuCategory")]
+        public IActionResult DeleteMenuCategory(int categoryId)
+        {
+            try
+            {
+                List<SqlParameter> parameters = new List<SqlParameter>
+        {
+            new SqlParameter("@category_id", SqlDbType.Int) { Value = categoryId }
+        };
+
+               
+                int rowsAffected = dataLayer.ExecuteSp_transaction("MenuCategory_DeleteCategory", parameters);
+
+                if (rowsAffected > 0)
+                {
+                    return Ok("Menu category deleted successfully.");
+                }
+                else
+                {
+                    return BadRequest("Failed to delete menu category.");
+                }
+            }
+            catch (Exception ex)
+            {
+              
+                new LogHelper().LogError("Error deleting menu category: " + ex.Message);
                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
             }
         }
--- DinePulse_API/DinePulse_API/Controllers/MenuController.cs
+++ DinePulse_API/DinePulse_API/Controllers/MenuController.cs
@@ -154,7 +154,7 @@ namespace DinePulse_API.Controllers
                 };
 
                 // Execute the stored procedure to insert the menu item
-                int rowsAffected = dataLayer.ExecuteSp_transaction("InsertMenu", parameters);
+                int rowsAffected = dataLayer.ExecuteSp_transaction("Menu_InsertMenu", parameters);
 
                 if (rowsAffected > 0)
                 {
@@ -173,7 +173,65 @@ namespace DinePulse_API.Controllers
             }
         }
 
+        [HttpPut]
+        [ActionName("UpdateMenuItem")]
+        public IActionResult UpdateMenuItem([FromBody] MenuModel menuModel)
+        {
+            try
+            {
+                byte[] imageBytes = Convert.FromBase64String(menuModel.ItemImageBase64);
 
-       
+                List<SqlParameter> parameters = new List<SqlParameter>
+        {
+            new SqlParameter("@item_id", SqlDbType.Int) { Value = menuModel.ItemId },
+            new SqlParameter("@item_name", SqlDbType.VarChar, 100) { Value = menuModel.ItemName },
+            new SqlParameter("@item_category", SqlDbType.Int) { Value = menuModel.ItemCategory },
+            new SqlParameter("@item_description", SqlDbType.VarChar, 255) { Value = menuModel.ItemDescription },
+            new SqlParameter("@item_price", SqlDbType.Decimal) { Value = menuModel.ItemPrice },
+            new SqlParameter("@item_image", SqlDbType.VarBinary, -1) { Value = imageBytes },
+        };
+
+               
+                int rowsAffected = dataLayer.ExecuteSp_transaction("Menu_UpdateMenu", parameters);
+
+                if (rowsAffected > 0)
+                {
+                    return Ok("Menu item updated successfully.");
+                }
+                else
+                {
+                    return BadRequest("Failed to update menu item.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                new LogHelper().LogError("Error updating menu item: " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+
+        [HttpDelete("{itemId}")]
+        [ActionName("DeleteMenuItem")]
+        public IActionResult DeleteMenuItem(int itemId)
+        {
+            try
+            {
+                List<SqlParameter> sp = new List<SqlParameter>()
+        {
+            new SqlParameter() { ParameterName = "@item_id", SqlDbType = SqlDbType.Int, Value = itemId }
+        };
+
+                dataLayer.ExecuteSp_transaction("DeleteMenuItem", sp);
+                return Ok("Menu item deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                new LogHelper().LogError("Error deleting menu item..." + ex.Message);
+                return BadRequest("Error deleting menu item. Please try again later.");
+            }
+        }
+
     }
 }
