7e481ded2b6c03a137115c56141972416043bef9
Joshy Johny <joshyjohny3530@gmail.com>
2024-06-06 23:28:26 -0400

    Producting adding to categrory

--- DinePulse_Admin/src/components/DashboardProducts.jsx
+++ DinePulse_Admin/src/components/DashboardProducts.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { MdAddToPhotos } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import { IoClose } from "react-icons/io5";
-import upload_image from './Assets/upload_image.png';
+import upload_image from "./Assets/upload_image.png";
 
 // Set the app element for accessibility
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export const DashboardProducts = () => {
   const [activeTab, setActiveTab] = useState("CATEGORIES");
@@ -18,58 +18,96 @@ export const DashboardProducts = () => {
   const [image, setImage] = useState(false);
   const [getMenuList, setMenuList] = useState([]);
 
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+  const [categoryImage, setCategoryImage] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("categoryModel.CategoryName", categoryName);
+    formData.append("categoryModel.CategoryDescription", categoryDescription);
+    formData.append("categoryImage", categoryImage);
+
+    try {
+      const response = await axios.post(
+        process.env.REACT_APP_API_URL + "MenuCategory/InsertCategory",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        toggleModalCategory();
+      } else {
+        setMessage("Failed to insert category.");
+      }
+    } catch (error) {
+      console.error("Error inserting category", error);
+      setMessage("An error occurred while processing your request.");
+    }
+  };
+
   const openTab = (tabName) => {
     setActiveTab(tabName);
   };
 
   const toggleModalCategory = () => {
     setIsModalOpenCategory(!isModalOpenCategory);
-    console.log('Category Modal Toggled:', !isModalOpenCategory);
+    console.log("Category Modal Toggled:", !isModalOpenCategory);
   };
 
   const toggleModalMenu = () => {
     setIsModalOpenMenu(!isModalOpenMenu);
-    console.log('Menu Modal Toggled:', !isModalOpenMenu);
+    console.log("Menu Modal Toggled:", !isModalOpenMenu);
   };
 
   useEffect(() => {
-    const API_URL = process.env.REACT_APP_API_URL+'MenuCategory/GetAllMenuCategories'
+    const API_URL =
+      process.env.REACT_APP_API_URL + "MenuCategory/GetAllMenuCategories";
     let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: API_URL,
-        headers: {}
+      method: "get",
+      maxBodyLength: Infinity,
+      url: API_URL,
+      headers: {},
     };
 
-    axios.request(config)
-        .then((response) => {
-            console.log('Response Data:', response.data); 
-            const data = response.data;     
-            setCategoryList(data);
-        })
-        .catch((error) => {
-            console.error('Caught error while fetching data:', error);
-        });
+    axios
+      .request(config)
+      .then((response) => {
+        console.log("Response Data:", response.data);
+        const data = response.data;
+        setCategoryList(data);
+      })
+      .catch((error) => {
+        console.error("Caught error while fetching data:", error);
+      });
   }, []);
 
   useEffect(() => {
-    const API_URL = process.env.REACT_APP_API_URL+'Menu/GetMenuAll'
+    const API_URL = process.env.REACT_APP_API_URL + "Menu/GetMenuAll";
     let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: API_URL,
-        headers: {}
+      method: "get",
+      maxBodyLength: Infinity,
+      url: API_URL,
+      headers: {},
     };
 
-    axios.request(config)
-        .then((response) => {
-            console.log('Response Data:', response.data); 
-            const data = response.data.data;      
-            setMenuList(data);
-        })
-        .catch((error) => {
-            console.error('Caught error while fetching data:', error); 
-        });
+    axios
+      .request(config)
+      .then((response) => {
+        console.log("Response Data:", response.data);
+        const data = response.data.data;
+        setMenuList(data);
+      })
+      .catch((error) => {
+        console.error("Caught error while fetching data:", error);
+      });
   }, []);
 
   return (
@@ -120,17 +158,24 @@ export const DashboardProducts = () => {
               <tbody>
                 {getCategoryList.map((categorylist) => (
                   <tr key={categorylist.categoryId}>
-                      <td>{categorylist.categoryId}</td>
-                      <td style={{TextAlign:'center'}}>
-                        {categorylist.categoryImageBase64 && <img src={`data:image/jpeg;base64,${categorylist.categoryImageBase64}`} className='categoryImage' alt={categorylist.categoryName} style={{ display: 'block', margin: '0 auto' }}/>}
-                      </td>
-                      <td>{categorylist.categoryName}</td>
-                      <td>{categorylist.categoryDescription}</td>
-                      <td>
-                          <FaEdit className='edit_icon'/>
-                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                          <RiDeleteBin5Fill className='delete_icon'/>
-                      </td>
+                    <td>{categorylist.categoryId}</td>
+                    <td style={{ TextAlign: "center" }}>
+                      {categorylist.categoryImageBase64 && (
+                        <img
+                          src={`data:image/jpeg;base64,${categorylist.categoryImageBase64}`}
+                          className="categoryImage"
+                          alt={categorylist.categoryName}
+                          style={{ display: "block", margin: "0 auto" }}
+                        />
+                      )}
+                    </td>
+                    <td>{categorylist.categoryName}</td>
+                    <td>{categorylist.categoryDescription}</td>
+                    <td>
+                      <FaEdit className="edit_icon" />
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <RiDeleteBin5Fill className="delete_icon" />
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -166,105 +211,169 @@ export const DashboardProducts = () => {
               </thead>
               <tbody>
                 {getMenuList.map((menulist) => (
-                    <tr key={menulist.item_id}>
-                        <td>{menulist.item_id}</td>
-                        <td>{menulist.item_name}</td>
-                        <td>{menulist.item_category}</td>
-                        <td>{menulist.item_description}</td>
-                        <td>${menulist.item_price}</td>
-                        <td>
-                            <FaEdit className='edit_icon'/>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <RiDeleteBin5Fill className='delete_icon'/>
-                        </td>
-                    </tr>
-                  ))}
+                  <tr key={menulist.item_id}>
+                    <td>{menulist.item_id}</td>
+                    <td>{menulist.item_name}</td>
+                    <td>{menulist.item_category}</td>
+                    <td>{menulist.item_description}</td>
+                    <td>${menulist.item_price}</td>
+                    <td>
+                      <FaEdit className="edit_icon" />
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <RiDeleteBin5Fill className="delete_icon" />
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
           <br />
         </div>
       </div>
-      <Modal isOpen={isModalOpenCategory} onRequestClose={toggleModalCategory} contentLabel="Add New Category"
-        className="modal" overlayClassName="modal-overlay">
-            <div className="modal-header">
-                <h2 className='modal-title'>Add New Category</h2>
-                <button className="modal-close-button" onClick={toggleModalCategory}>
-                    <IoClose />
-                </button>
+      <Modal
+        isOpen={isModalOpenCategory}
+        onRequestClose={toggleModalCategory}
+        contentLabel="Add New Category"
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <div className="modal-header">
+          <h2 className="modal-title">Add New Category</h2>
+          <button className="modal-close-button" onClick={toggleModalCategory}>
+            <IoClose />
+          </button>
+        </div>
+        <div className="add">
+          <form className="flex-col" onSubmit={handleSubmit}>
+            <div className="add-category-img-upload flex-col">
+              <p>Upload Image</p>
+              <label htmlFor="image">
+                <img
+                  src={
+                    categoryImage
+                      ? URL.createObjectURL(categoryImage)
+                      : upload_image
+                  }
+                  alt=""
+                />
+              </label>
+              <input
+                onChange={(e) => setCategoryImage(e.target.files[0])}
+                type="file"
+                id="image"
+                required
+              />
             </div>
-            <div className='add'>
-                <form className='flex-col'>
-                    <div className='add-category-img-upload flex-col'>
-                        <p>Upload Image</p>
-                        <label htmlFor='image'>
-                            <img src={image ? URL.createObjectURL(image) : upload_image} alt='' />
-                        </label>
-                        <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='image' hidden required />
-                    </div>
-                    <div className='add-category-name flex-col'>
-                        <p>Category Name</p>
-                        <input type='text' name='name' placeholder='Type here'/>
-                    </div>
-                    <div className='add-category-description flex-col'>
-                        <p>Category Description</p>
-                        <textarea name='description' placeholder='Write content here' required/>
-                    </div>
-                    <div className='category-buttons'>
-                        <button type='submit' className='add-btn'>ADD</button>
-                        <button type='button' className='cancel-btn' onClick={toggleModalCategory}>CANCEL</button>
-                    </div>
-                </form>
+            <div className="add-category-name flex-col">
+              <p>Category Name</p>
+              <input
+                type="text"
+                name="name"
+                placeholder="Type here"
+                value={categoryName}
+                onChange={(e) => setCategoryName(e.target.value)}
+                required
+              />
             </div>
+            <div className="add-category-description flex-col">
+              <p>Category Description</p>
+              <textarea
+                name="description"
+                placeholder="Write content here"
+                value={categoryDescription}
+                onChange={(e) => setCategoryDescription(e.target.value)}
+                required
+              />
+            </div>
+            <div className="category-buttons">
+              <button type="submit" className="add-btn">
+                ADD
+              </button>
+              <button
+                type="button"
+                className="cancel-btn"
+                onClick={toggleModalCategory}
+              >
+                CANCEL
+              </button>
+            </div>
+          </form>
+        </div>
       </Modal>
 
-      <Modal isOpen={isModalOpenMenu} onRequestClose={toggleModalMenu} contentLabel="Add New MenuItem"
-        className="modal" overlayClassName="modal-overlay">
-            <div className="modal-header">
-                <h2 className='modal-title'>Add New MenuItem</h2>
-                <button className="modal-close-button" onClick={toggleModalMenu}>
-                    <IoClose />
-                </button>
+      <Modal
+        isOpen={isModalOpenMenu}
+        onRequestClose={toggleModalMenu}
+        contentLabel="Add New MenuItem"
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <div className="modal-header">
+          <h2 className="modal-title">Add New MenuItem</h2>
+          <button className="modal-close-button" onClick={toggleModalMenu}>
+            <IoClose />
+          </button>
+        </div>
+        <div className="add">
+          <form className="flex-col">
+            <div className="add-product-img-upload flex-col">
+              <p>Upload Image</p>
+              <label htmlFor="image">
+                <img
+                  src={image ? URL.createObjectURL(image) : upload_image}
+                  alt=""
+                />
+              </label>
+              <input
+                onChange={(e) => setImage(e.target.files[0])}
+                type="file"
+                id="image"
+                hidden
+                required
+              />
             </div>
-            <div className='add'>
-                <form className='flex-col'>
-                    <div className='add-img-upload flex-col'>
-                        <p>Upload Image</p>
-                        <label htmlFor='image'>
-                            <img src={image ? URL.createObjectURL(image) : upload_image} alt='' />
-                        </label>
-                        <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='image' hidden required />
-                    </div>
-                    <div className='add-product-name flex-col'>
-                        <p>Product Name</p>
-                        <input type='text' name='name' placeholder='Type here'/>
-                    </div>
-                    <div className='add-product-description flex-col'>
-                        <p>Product Description</p>
-                        <textarea name='description' placeholder='Write content here' required/>
-                    </div>
-                    <div className='add-category flex-col'>
-                        <p>Product Category</p>
-                        <select name='category'>
-                            <option value="#">Select Category</option>
-                            <option value="aaaa">aaaa</option>
-                            <option value="bbbb">bbbb</option>
-                            <option value="cccc">cccc</option>
-                            <option value="dddd">dddd</option>
-                            <option value="eeee">eeee</option>
-                        </select>
-                    </div>
-                    <div className='add-price flex-col'>
-                        <p>Product Price</p>
-                        <input type='number' name='price' placeholder='$20'/>
-                    </div>
-                    <div className='section-buttons'>
-                        <button type='submit' className='add-btn'>ADD</button>
-                        <button type='button' className='cancel-btn' onClick={toggleModalMenu}>CANCEL</button>
-                    </div>
-                </form>
+            <div className="add-product-name flex-col">
+              <p>Product Name</p>
+              <input type="text" name="name" placeholder="Type here" />
             </div>
-        </Modal>
+            <div className="add-product-description flex-col">
+              <p>Product Description</p>
+              <textarea
+                name="description"
+                placeholder="Write content here"
+                required
+              />
+            </div>
+            <div className="add-category flex-col">
+              <p>Product Category</p>
+              <select name="category">
+                <option value="#">Select Category</option>
+                <option value="aaaa">aaaa</option>
+                <option value="bbbb">bbbb</option>
+                <option value="cccc">cccc</option>
+                <option value="dddd">dddd</option>
+                <option value="eeee">eeee</option>
+              </select>
+            </div>
+            <div className="add-price flex-col">
+              <p>Product Price</p>
+              <input type="number" name="price" placeholder="$20" />
+            </div>
+            <div className="section-buttons">
+              <button type="submit" className="add-btn">
+                ADD
+              </button>
+              <button
+                type="button"
+                className="cancel-btn"
+                onClick={toggleModalMenu}
+              >
+                CANCEL
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
     </main>
   );
 };
