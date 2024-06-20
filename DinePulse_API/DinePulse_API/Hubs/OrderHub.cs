dbf50db6329e99c137076d40c27b696c16aa2874
NibinByteCode <nibinm365@gmail.com>
2024-06-20 07:37:51 -0400

    Add SignalR dependency

---
+++ DinePulse_API/DinePulse_API/Hubs/OrderHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DinePulse_API.Hubs
+{
+    public class OrderHub:Hub
+    {
+        public OrderHub() { }
+    }
+}
--- DinePulse_API/DinePulse_API/Program.cs
+++ DinePulse_API/DinePulse_API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSignalR();
 // Add CORS policy
 builder.Services.AddCors(options =>
 {
