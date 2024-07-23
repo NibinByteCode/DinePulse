1e496217eb863f42cfb9a6f103bbf804bcd44261
aimyshaju2526 <aimyshaju2526@gmail.com>
2024-07-23 18:20:01 -0400

    Modify Login, Register Designs

--- DinePulse_Customer/dinepulse_customer/src/components/Register.jsx
+++ DinePulse_Customer/dinepulse_customer/src/components/Register.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Register.css";
 import registerImage from "../assets/register_image.png";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneVolume } from "react-icons/fa6";
 
 const Register = () => {
   return (
@@ -11,26 +14,32 @@ const Register = () => {
       </div>
       <div className="right-section">
         <h2>LET'S GET STARTED...</h2>
+        <br/><br/>
         <form>
           <div className="input-group">
             <input type="text" placeholder="Customer Name" required />
-            <i className="icon-user"></i>
+            {/*<i className="icon-user"></i>*/}
+            <FaUserAlt className="icons" />
           </div>
           <div className="input-group">
             <input type="email" placeholder="Email id" required />
-            <i className="icon-email"></i>
+            {/*<i className="icon-email"></i>*/}
+            <MdEmail className="icons" />
           </div>
           <div className="input-group">
             <input type="password" placeholder="New password" required />
-            <i className="icon-password"></i>
+            {/*<i className="icon-password"></i>*/}
+            <FaLock className="icons" />
           </div>
           <div className="input-group">
             <input type="password" placeholder="Confirm password" required />
-            <i className="icon-password"></i>
+            {/*<i className="icon-password"></i>*/}
+            <FaLock className="icons" />
           </div>
           <div className="input-group">
             <input type="tel" placeholder="Contact Number" required />
-            <i className="icon-phone"></i>
+            {/*<i className="icon-phone"></i>*/}
+            <FaPhoneVolume className="icons" />
           </div>
           <button type="submit" className="btn-register">
             REGISTER
