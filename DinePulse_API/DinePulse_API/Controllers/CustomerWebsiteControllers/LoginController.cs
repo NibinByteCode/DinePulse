9fc944a713891cc7dbc6f688249a34c72920b746
NibinByteCode <nibinm365@gmail.com>
2024-07-23 22:14:44 -0400

    Add Controller for Login,Register and Table Reservation

---
+++ DinePulse_API/DinePulse_API/Controllers/CustomerWebsiteControllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DinePulse_API.Controllers.CustomerWebsiteControllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
