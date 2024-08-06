83db452203d9106030ba363735667e5fc7e357fa
NibinByteCode <nibinm365@gmail.com>
2024-08-05 23:37:11 -0400

    Add API Endpoint for Get Table with Status for Mobile

--- DinePulse_API/DinePulse_API/Controllers/MobileControllers/MobileOrderController.cs
+++ DinePulse_API/DinePulse_API/Controllers/MobileControllers/MobileOrderController.cs
@@ -5,6 +5,8 @@ using DinePulse_API.Database;
 using Microsoft.Extensions.Configuration;
 using DinePulse_API.Models;
 using System.Text.Json;
+using DinePulse_API.Utils;
+using Newtonsoft.Json;
 
 namespace DinePulse_API.Controllers.MobileControllers
 {
@@ -26,28 +28,30 @@ namespace DinePulse_API.Controllers.MobileControllers
         {
             try
             {
-                List<SqlParameter> parameters = new List<SqlParameter>
+                List<SqlParameter> sp = new List<SqlParameter>()
                 {
-                    new SqlParameter("@customerId", orderRequest.CustomerId),
-                    new SqlParameter("@tableId", orderRequest.TableId),
-                    new SqlParameter("@orderTypeId", orderRequest.OrderTypeId),
-                    new SqlParameter("@statusId", orderRequest.StatusId),
-                    new SqlParameter("@orderDetails", JsonSerializer.Serialize(orderRequest.OrderDetails))
+                    new SqlParameter() { ParameterName = "@customerId", SqlDbType = SqlDbType.Int, Value = orderRequest.CustomerId },
+                    new SqlParameter() { ParameterName = "@tableId", SqlDbType = SqlDbType.Int, Value = orderRequest.TableId },
+                    new SqlParameter() { ParameterName = "@orderTypeId", SqlDbType = SqlDbType.Int, Value = orderRequest.OrderTypeId },
+                    new SqlParameter() { ParameterName = "@statusId", SqlDbType = SqlDbType.Int, Value = orderRequest.StatusId },
+                    new SqlParameter() { ParameterName = "@orderDetails", SqlDbType = SqlDbType.NVarChar, Value =  JsonConvert.SerializeObject(orderRequest.OrderDetails) }
                 };
-                int result = await dataLayer.ExecuteNonQueryAsync("Order_InsertDetails", parameters);
-                if (result > 0)
+
+                var result = await dataLayer.ExecuteInsertAsync("Order_InsertDetails", sp);
+
+                if (result.Result == 1)
                 {
-                    return Ok(new { message = "Order created successfully" });
+                    return Ok("Order added successfully");
                 }
                 else
                 {
-                    return StatusCode(500, new { message = "Failed to create order" });
+                    return BadRequest($"Error adding order: {result.ErrorMessage}");
                 }
-
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { message = "Failed to create order."+ex.Message });
+                new LogHelper().LogError("Error adding order..." + ex.Message);
+                return BadRequest("Error adding order. Please try again later.");
             }
         }
     }
