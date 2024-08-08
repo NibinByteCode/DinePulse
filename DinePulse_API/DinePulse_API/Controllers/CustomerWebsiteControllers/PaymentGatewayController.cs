5eee730f1b3c58ddcb411906ae25df641a6026a7
NibinByteCode <nibinm365@gmail.com>
2024-08-08 02:48:41 -0400

    Add API Endpoint using Stripe

---
+++ DinePulse_API/DinePulse_API/Controllers/CustomerWebsiteControllers/PaymentGatewayController.cs
@@ -0,0 +1,36 @@
+﻿using DinePulse_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Forwarding;
+
+namespace DinePulse_API.Controllers.CustomerWebsiteControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentGatewayController : Controller
+    {
+
+        public PaymentGatewayController()
+        {
+
+            StripeConfiguration.ApiKey = "sk_test_51PlO8JGXMgxB8JculPNhhXaCY26lXdpsxA8GATjjmwk8JPdOIKo1Fh64AnhbEmgj4wySORvqzjrNWWqadGVQgOAa006uFwdoA4";
+        }
+
+        [HttpPost]
+        [ActionName("MakePayment")]
+        public IActionResult CreatePayment([FromBody] PaymentRequestModel paymentrequest)
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = paymentrequest.Amount,
+                Currency = "cad",
+                PaymentMethodTypes = new List<string> { "card" },
+            };
+            var service = new PaymentIntentService();
+            var paymentIntent = service.Create(options);
+
+            return Ok(new { clientSecret = paymentIntent.ClientSecret });
+        }
+    }
+}
+
---
+++ DinePulse_API/DinePulse_API/Models/PaymentRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DinePulse_API.Models
+{
+    public class PaymentRequestModel
+    {
+        public long Amount { get; set; }
+    }
+
+}
