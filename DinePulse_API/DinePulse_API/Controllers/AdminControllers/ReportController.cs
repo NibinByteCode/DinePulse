f74062de9cc3f8832bffb7cb7b9c68a3f73ad974
NibinByteCode <nibinm365@gmail.com>
2024-08-15 15:37:43 -0400

    Add API End point for Monthly sales report

--- DinePulse_API/DinePulse_API/Controllers/AdminControllers/ReportController.cs
+++ DinePulse_API/DinePulse_API/Controllers/AdminControllers/ReportController.cs
@@ -56,5 +56,46 @@ namespace DinePulse_API.Controllers.AdminControllers
                 return BadRequest("No Data Fetched...Please Try Later");
             }
         }
+
+
+        [HttpGet]
+        [ActionName("GetSalesReportMonthly")]
+        public IActionResult GetSalesReportMonthly(int ReportYear, int ReportMonth)
+        {
+            try
+            {
+                List<SqlParameter> sp = new List<SqlParameter>()
+                {
+                 new SqlParameter() { ParameterName = "@ReportMonth", SqlDbType = SqlDbType.Int, Value = ReportMonth },
+                 new SqlParameter() { ParameterName = "@ReportYear", SqlDbType = SqlDbType.Int, Value = ReportYear }
+                };
+
+                DataTable table = dataLayer.Getbulkfromdb("[Reports_TotalSaleReport_Monthly]", sp);
+
+                if (table.Rows.Count > 0)
+                {
+
+                    string jsonResult = table.Rows[0][0].ToString();
+
+                    if (!string.IsNullOrEmpty(jsonResult))
+                    {
+                        return Content(jsonResult, "application/json");
+                    }
+                    else
+                    {
+                        return NotFound(); 
+                    }
+                }
+                else
+                {
+                    return NotFound(); 
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogHelper().LogError("Error getting data..." + ex.Message);
+                return BadRequest("No Data Fetched...Please Try Later");
+            }
+        }
     }
 }
