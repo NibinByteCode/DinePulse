e85de1e8c8ceb839315def4486dcf1b39f65740a
NibinByteCode <nibinm365@gmail.com>
2024-08-15 14:58:53 -0400

    Add API End Point for Sales Report by date

--- DinePulse_API/DinePulse_API/Controllers/AdminControllers/ReportController.cs
+++ DinePulse_API/DinePulse_API/Controllers/AdminControllers/ReportController.cs
@@ -1,11 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DinePulse_API.Database;
+using DinePulse_API.Utils;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DinePulse_API.Controllers.AdminControllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ReportController : ControllerBase
     {
+        DataLayer dataLayer;
+        readonly IConfiguration _iconfiguration;
+        public ReportController(IConfiguration iconfiguration)
+        {
+            _iconfiguration = iconfiguration;
+            dataLayer = new DataLayer(_iconfiguration);
+        }
+        [HttpGet]
+        [ActionName("GetSalesReport")]
+        public IActionResult GetSalesReport(string reportdate)
+        {
+            try
+            {
+                List<SqlParameter> sp = new List<SqlParameter>()
+                {
+                 new SqlParameter() { ParameterName = "@ReportDate", SqlDbType = SqlDbType.Date, Value = reportdate }
+                };
+
+                DataTable table = dataLayer.Getbulkfromdb("Reports_TotalSaleReport", sp);
+
+                if (table.Rows.Count > 0)
+                {
+
+                    string jsonResult = table.Rows[0][0].ToString();
+
+                    if (!string.IsNullOrEmpty(jsonResult))
+                    {
+                        return Content(jsonResult, "application/json");
+                    }
+                    else
+                    {
+                        return NotFound(); // No data found
+                    }
+                }
+                else
+                {
+                    return NotFound(); // No data found
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogHelper().LogError("Error getting data..." + ex.Message);
+                return BadRequest("No Data Fetched...Please Try Later");
+            }
+        }
     }
 }
